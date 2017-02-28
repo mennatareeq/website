0b11cb902ad3085098e76187bbd80bf1e9c14c7a
manarsherif <manar.sherif93@gmail.com>
2017-02-28 23:03:11 +0200

    table success

--- photos/views.py
+++ photos/views.py
@@ -10,26 +10,23 @@ class Test(generic.DetailView):
 	template_name='photos/test.html'
 
 
-class IndexView(generic.ListView):
-	template_name = 'photos/index.html'
-	#context_object_name = 'all_photos'
 
-	def get_queryset(self):
-		list_of_lists=[]
-		objects_5=[]
-		for photo in Photo.objects.all():
-			if len(objects_5)<5 :
-				objects_5.append(photo)
-			else:
-				list_of_lists.append(objects_5)
-				objects_5=[]
-				objects_5.append(photo)
-		if(len(objects_5)<5):
+def index(request):
+	list_of_lists=[]
+	objects_5=[]
+	for photo in Photo.objects.all():
+		if len(objects_5)<5 :
+			objects_5.append(photo)
+		else:
 			list_of_lists.append(objects_5)
-		print len(list_of_lists)
-		for list in list_of_lists:
-			print len(list)
-		return list_of_lists
+			objects_5=[]
+			objects_5.append(photo)
+	if(len(objects_5)<5):
+		list_of_lists.append(objects_5)
+	print len(list_of_lists)
+	for list in list_of_lists:
+		print len(list)
+	return render(request,'photos/index.html',{'list_of_lists':list_of_lists})
 
 
 
